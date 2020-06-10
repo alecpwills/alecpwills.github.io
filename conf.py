4da9c5d675c6012e374c4b9282db93a9706267e0
alecpwills <alec.wills@stonybrook.edu>
2020-06-09 20:26:27 -0400

    update redirect

--- conf.py
+++ conf.py
@@ -624,7 +624,7 @@ ATOM_FILENAME_BASE = "feed"
 # relative URL.
 #
 # If you don't need any of these, just set to []
-REDIRECTIONS = [('/', '/home/index.html')]
+REDIRECTIONS = []
 
 # Presets of commands to execute to deploy. Can be anything, for
 # example, you may use rsync:
